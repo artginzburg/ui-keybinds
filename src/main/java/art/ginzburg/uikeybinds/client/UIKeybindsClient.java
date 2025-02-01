2bd9f82421cccc4ac6853e084b255448909f3ad3
Art Ginzburg <ginzburg.arthur@gmail.com>
2025-02-01 05:07:18 +0400

    feat: resource pack

--- src/main/java/art/ginzburg/uikeybinds/client/UIKeybindsClient.java
+++ src/main/java/art/ginzburg/uikeybinds/client/UIKeybindsClient.java
@@ -1,11 +1,21 @@
 package art.ginzburg.uikeybinds.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 
 public class UIKeybindsClient implements ClientModInitializer {
+  public static final String ID = "uikeybinds";
 
   @Override
   public void onInitializeClient() {
-
+    FabricLoader.getInstance().getModContainer(ID).ifPresent(modContainer -> {
+      ResourceManagerHelper.registerBuiltinResourcePack(ResourceLocation.parse("uikeybinds"), modContainer,
+          Component.literal("UI Keybinds"),
+          ResourcePackActivationType.DEFAULT_ENABLED);
+    });
   }
 }
